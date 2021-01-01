3a99e7a22595abc503150629a5fad9d308bf4198
Michdo93 <michaeldoerflinger93@gmail.com>
2021-01-01 21:50:02 +0100

    Create rpm.py

---
+++ rpm.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO
+import time
+
+sensor = 21
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensor, GPIO.IN)
+
+sample = 100
+count = 0
+
+start = 0
+end = 0
+
+def set_start():
+    global start
+    start = time.time()
+
+
+def set_end():
+    global end
+    end = time.time()
+
+
+def get_rpm(self):
+    global count
+
+    if not count:
+            set_start()
+            count = count + 1
+
+    else:
+            count = count + 1
+
+            if count == sample:
+                set_end()
+                delta = end - start
+                delta = delta / 60
+                rpm = (sample / delta)
+                print (round(rpm,0))
+                count = 0
+                return 
+
+GPIO.add_event_detect(sensor,GPIO.RISING, callback=get_rpm)
+
+try:
+        while True:
+            time.sleep(0.1)
+except KeyboardInterrupt:
+            print ("   Quit")
+            GPIO.cleanup()
